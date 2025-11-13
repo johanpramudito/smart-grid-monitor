88fe9cc82566e675bd1005d8cd57f6a43f722b0b
johanpramudito <ajohanpramudito@gmail.com>
2025-11-13 14:39:48 +0700

    fix: package

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Required for Azure App Service (non-serverless)
 };
 
 export default nextConfig;
